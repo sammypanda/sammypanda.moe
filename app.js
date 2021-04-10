402f30346db25cedf66f5a2577d3dee654b23f51
BuildTools <sammy0penguin@gmail.com>
2021-04-10 11:26:46 +1000

    mobile friendly nav <3

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
+function resetGoto(omit) {
+    const allGotos = document.getElementsByClassName('nav')
+
+    for(var i = 0; i < allGotos.length; i++) {
+        if (i !== omit) {
+            allGotos[i].style.visibility = "hidden"
+        } else {
+            allGotos[i].style.visibility = "visible"
+        }
+    }
+}
+
 info = (container, project) => {
     var info = document.getElementById('info')
     switch (project) {
         case "current":
             info.innerHTML = "I'm the portfolio page you're on right now, I was made from absolute scratch!"
-            container.setAttribute("href", "https://github.com/Samdvich/sammypanda.moe")
-            container.childNodes[0].src = "assets/img/meta/favicon.ico"
+            container.childNodes[3].setAttribute("href", "https://github.com/Samdvich/sammypanda.moe")
+            container.childNodes[1].src = "assets/img/meta/favicon.ico"
+            resetGoto(0)
             break
         case "compui":
             info.innerHTML = "I'm a web application for schools with team-based competitive events. I was built for Shailer Park State High School's use. You can try me out with U:admin@eq.edu.au P:admin.%.account. Here's my <a href='https://github.com/Samdvich/CompUI'>source code</a>"
-            container.setAttribute("href", "http://shaiparkshs.ipto.com.au/11%20Digital%20Solutions/spage65/CompUI/login.php")
-            container.childNodes[0].src = "assets/img/project/compui.png"
+            container.childNodes[3].setAttribute("href", "http://shaiparkshs.ipto.com.au/11%20Digital%20Solutions/spage65/CompUI/login.php")
+            container.childNodes[1].src = "assets/img/project/compui.png"
+            resetGoto(1)
             break
         case "murb":
             info.innerHTML = "I'm Murb (my full name is \"Music Curb\"), I'm an <a href='https://github.com/Samdvich/murb'>open source</a> music broadcasting platform. I'm also Sammy's first time using typescript and his second time using Shell."
-            container.setAttribute("href", "https://github.com/Samdvich/murb")
-            container.childNodes[0].src = "assets/img/project/murb.png"
+            container.childNodes[3].setAttribute("href", "https://github.com/Samdvich/murb")
+            container.childNodes[1].src = "assets/img/project/murb.png"
+            resetGoto(2)
             break
         case "coderacer":
             info.innerHTML = "I'm a time-based quiz game, around basic web development knowledge. I'm also Sammy's first PHP project!"
-            container.setAttribute("href", "https://github.com/Samdvich/CodeRacer")
-            container.childNodes[0].src = "assets/img/project/coderacer.png"
+            container.childNodes[3].setAttribute("href", "https://github.com/Samdvich/CodeRacer")
+            container.childNodes[1].src = "assets/img/project/coderacer.png"
+            resetGoto(3)
             break
         case "dragonsgauntlet":
             info.innerHTML = "Sammy was called in by a past teacher to help on Dragons Gauntlet, it's a fully-fledged LARP interface and event manager, built with React and Typescript and translated to Vue/NuxtJS! But this project is sadly locked away as the development is proprietary"
-            container.setAttribute("href", "https://github.com/Samdvich/DragonsGauntlet/tree/typescript")
-            container.childNodes[0].src = "assets/img/project/dragonsgauntlet.png"
+            container.childNodes[3].setAttribute("href", "https://github.com/Samdvich/DragonsGauntlet/tree/typescript")
+            container.childNodes[1].src = "assets/img/project/dragonsgauntlet.png"
+            resetGoto(4)
             break
         default:
             info.innerHTML = ""
